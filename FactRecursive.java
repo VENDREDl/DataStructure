03c06b5e901fc5de8634e6355400b1eebf2eaa67
VENDREDl <65541546+VENDREDl@users.noreply.github.com>
2021-03-03 16:04:39 +0900

    FactRecursive.java 수정

--- FactRecursive.java
+++ FactRecursive.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class FactRecursive {
 
 	public static void main(String[] args) {
-		
+	
 		Scanner sc = new Scanner(System.in);
 		
 		int num = sc.nextInt();
-		int fac = 1;
-		
-		for(int i = num; i >= 1; i--) {
-			fac = fac * i;
-		}
-		
-		System.out.println(num + "의 팩토리얼은" + fac + "입니다.");
+		System.out.print(fact(num));
+}
+	
+	public static int fact(int n) {
+		if(n <= 1)
+			return n;
+		else
+			return fact(n-1) * n;
 	}
 
 }
